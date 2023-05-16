a59fdea405bad295883e60d0b6c0ca257aab0334
Николай Михайлович Казаковцев <75414235+Acidosulus@users.noreply.github.com>
2023-05-16 22:58:30 +0700

    Add files via upload

---
+++ day_1/test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+curl -i -X GET "http://127.0.0.1:1234/First"
+
+curl -i -X GET "http://127.0.0.1:1234/Second"
+
+curl -i -X GET "http://127.0.0.1:1234/Summa"
+
+curl -i -X GET "http://127.0.0.1:1234"

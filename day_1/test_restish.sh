3b626cb2612769fe219719f08d6611010210003d
Николай Михайлович Казаковцев <75414235+Acidosulus@users.noreply.github.com>
2023-05-17 00:31:49 +0700

    Add files via upload

---
+++ day_1/test_restish.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+restish GET "http://127.0.0.1:1234/First"
+
+restish GET "http://127.0.0.1:1234/Second"
+
+restish GET "http://127.0.0.1:1234/Summa"
+
+restish GET "http://127.0.0.1:1234"
